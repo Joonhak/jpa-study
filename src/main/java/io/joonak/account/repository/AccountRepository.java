d438682d78885d3c8c7ae4d0e4f9ecd8ab0ce0b8
Joonhak <ofnullable@gmail.com>
2019-08-16 19:39:46 +0900

    Add querydsl predicate executor at account repository

--- src/main/java/io/joonak/account/repository/AccountRepository.java
+++ src/main/java/io/joonak/account/repository/AccountRepository.java
@@ -3,10 +3,11 @@ package io.joonak.account.repository;
 import io.joonak.account.domain.Account;
 import io.joonak.account.domain.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.Optional;
 
-public interface AccountRepository extends JpaRepository<Account, Long>, AccountCustomRepository {
+public interface AccountRepository extends JpaRepository<Account, Long>, AccountCustomRepository, QuerydslPredicateExecutor<Account> {
 
     boolean existsByEmail(Email email);
 
